73599e65e9d54e449e1c1b3dfe5acd5644f1edde
Nanfe05 <hernan05felipe@gmail.com>
2020-04-13 09:15:22 -0500

    fix http request proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000/api',
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
