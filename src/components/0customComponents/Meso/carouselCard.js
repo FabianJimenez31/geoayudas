5fe4924fb7c5f395576c79830ec1b63b3e4da681
Nanfe05 <hernan05felipe@gmail.com>
2020-05-02 15:56:37 -0500

    xs screens css

--- src/components/0customComponents/Meso/carouselCard.js
+++ src/components/0customComponents/Meso/carouselCard.js
@@ -9,13 +9,13 @@ import {ToStrong} from '../commonFunctions';
 // This component will display mainly Text
 const CarouselCard = ({content}) => {
     return <Fragment>
-         <Col className={`carousel-text1`} span={24}>
+         <Col className={`carousel-text1`} span={24} style={{margin:'10px'}}>
                 <span>{content.text1}</span>
             </Col>
-            <Col className={`carousel-text2`} span={24}>
+            <Col className={`carousel-text2`} span={24} >
                 <h1 className='home-title'>{content.text2}</h1>
             </Col>
-            <Col className={`carousel-text3`} span={24}>
+            <Col className={`carousel-text3`} span={24} style={{padding:'10px'}}>
                 <p>
                     {ToStrong(content.text3)}
                 </p>
